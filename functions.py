855997303cc9529842d742dbffaed667b795a3c2
febrifahmi <fahmi_fafa@yahoo.com>
2020-02-08 21:08:43 +0700

    Updating all code.

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # --------------------------------------------------------------------------------------
 # TRFM is written by @febrifahmi :c 2020
 # Toll Road Flood Monitoring system for Indonesia toll road crossdrain flood monitoring
@@ -6,4 +7,17 @@
 # functions.py : code for base image processing.
 # --------------------------------------------------------------------------------------
 
-import datetime
+from datetime import datetime,timedelta
+
+# get datetime and time now in UTC and GMT
+def gettargettime():
+	targetdatetime = datetime.utcnow() - timedelta(minutes=10)
+	formatteddatetime = targetdatetime.strftime("%Y-%m-%d %H:%M:%S")
+	formattedtime = targetdatetime.strftime("%H%M")
+	targetgmttime = datetime.now() - timedelta(minutes=10)
+	formattedtimeGMT = targetgmttime.strftime("%H:%M:%S")
+	return formatteddatetime, formattedtime, formattedtimeGMT
+
+# to do:
+# buat fungsi delete old sat images untuk menghemat space
+# 
--- trfm.py
+++ trfm.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # --------------------------------------------------------------------------------------
 # TRFM is written by @febrifahmi :c 2020
 # Toll Road Flood Monitoring system for Indonesia toll road crossdrain flood monitoring
 # using OpenCV
-# trfm.py : base code for the TRFM analysis.
+# trfm.py : base code for TRFM analysis that will be run every 10 minutes using cron
 # --------------------------------------------------------------------------------------
 
 import cv2
 import math
-import numpy as np 
-from matplotlib import pyplot as plt 
+import numpy as np
+from matplotlib import pyplot as plt
+import functions as fn
 
+# i'm using se3 hrp here, means "South East Asia 3 area" - "Heavy Rainfall Potential"
 himawaribase = 'https://www.data.jma.go.jp/mscweb/data/himawari/img/se3/'
+imagename = "{}" .format(fn.gettargettime()[1])
+downloadURL = himawaribase + "se3_hrp_" + imagename + ".jpg"
+
+print(downloadURL)
+print(fn.gettargettime()[2])
