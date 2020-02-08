14aef2aa2be75599b21d51fa410a743e35d61c4a
febrifahmi <fahmi_fafa@yahoo.com>
2020-02-08 12:06:55 +0700

    Initial commit.

---
+++ trfm.py
@@ -0,0 +1,11 @@
+# --------------------------------------------------------------------------------------
+# TRFM is written by @febrifahmi
+# Toll Road Flood Monitoring system for Indonesia toll road crossdrain flood monitoring
+# --------------------------------------------------------------------------------------
+
+#!/usr/bin/env python
+import cv2
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+himawaribase = 'https://www.data.jma.go.jp/mscweb/data/himawari/img/se3/'
