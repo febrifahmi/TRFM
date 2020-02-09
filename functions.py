ed1c4edc85404725a86e63496e566dae43a255ba
febrifahmi <fahmi_fafa@yahoo.com>
2020-02-09 12:29:27 +0700

    Revised functions.py and trfm.py

--- functions.py
+++ functions.py
@@ -7,7 +7,8 @@
 # functions.py : code for base image processing.
 # --------------------------------------------------------------------------------------
 
-
+import os
+import requests
 from datetime import datetime,timedelta
 
 # get datetime and time now in UTC and GMT
@@ -19,8 +20,35 @@ def gettargettime():
 	formattedtimeGMT = targetgmttime.strftime("%H:%M:%S")
 	return formatteddatetime, formattedtime, formattedtimeGMT
 
+# i'm using se3 hrp here, means "South East Asia 3 area" - "Heavy Rainfall Potential"
+himawaribase = 'https://www.data.jma.go.jp/mscweb/data/himawari/img/se3/'
+imagename = "/se3_hrp_" + "{}" .format(gettargettime()[1]) + ".jpg"
+downloadURL = himawaribase + imagename
 
+# set saved image folder
+imgfolder = os.path.abspath(os.path.join(os.path.dirname('TRFM'), '..', 'images'))
+savedimgname = imgfolder + imagename
 
+# download himawari sat image
+def downloadhimawari():
+	with open(savedimgname, 'wb') as f:
+	    f.write(requests.get(downloadURL).content)
+
+# remove all image files in folder if time reached 00:03:00 UTC (or the next day), so we can start with empty and fresh data
+def clearimgfolder():
+	if gettargettime()[1] == '0003':
+		for file in os.listdir(imgfolder):
+		    file_path = os.path.join(imgfolder, file)
+		    try:
+		        if os.path.isfile(file_path) or os.path.islink(file_path):
+		            os.unlink(file_path)
+		        elif os.path.isdir(file_path):
+		            shutil.rmtree(file_path)
+		    except Exception as e:
+		        print('Failed to delete %s. Reason: %s' % (file_path, e))
+	else:
+		print('Not cleaning. Not reached 0003 yet.  Pass.')
+		pass
 
 # to do:
 # buat fungsi delete old sat images untuk menghemat space
--- trfm.py
+++ trfm.py
@@ -9,31 +9,16 @@
 
 import cv2
 import math
-import os
-import requests
 import numpy as np
 from matplotlib import pyplot as plt
 import functions as fn
 
-# set saved image folder
-imgfolder = os.path.abspath(os.path.join(os.path.dirname('TRFM'), '..', 'images'))
-
-# i'm using se3 hrp here, means "South East Asia 3 area" - "Heavy Rainfall Potential"
-himawaribase = 'https://www.data.jma.go.jp/mscweb/data/himawari/img/se3/'
-imagename = "/se3_hrp_" + "{}" .format(fn.gettargettime()[1]) + ".jpg"
-downloadURL = himawaribase + imagename
-
-savedimgname = imgfolder + imagename
-
-# download himawari sat image
-def downloadhimawari():
-	with open(savedimgname, 'wb') as f:
-	    f.write(requests.get(downloadURL).content)
-
-print(downloadURL)
+print(fn.downloadURL)
 print(fn.gettargettime()[2])
-print(imgfolder)
-downloadhimawari()
+print(fn.imgfolder)
+fn.downloadhimawari()
+# check if the time reach 00:03:00 and clean the images in folder
+fn.clearimgfolder()
 
 # read the satellite image using numpy
 # img = cv2.imread('a.jpg',0)
